8a389e2fe0e10d8573ae4b3c27541be7a15dd0f7
sharo <sharo69@gmail.com>
2015-09-11 21:48:56 +0300

    are e

--- src/main/java/register/RegisterApplication.java
+++ src/main/java/register/RegisterApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RegisterApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args ) {
         SpringApplication.run(RegisterApplication.class, args);
     }
 }
